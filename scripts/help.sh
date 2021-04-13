d978f91f582d6c176ab4e703c98dd9d44f635c0d
Justin Porteous <justin@cloudcoders.co.za>
2021-04-13 20:23:46 +0200

    Update Help File

--- scripts/help.sh
+++ scripts/help.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-cat /usr/share/eiab/files/help
+echo EchoLink in a Box by ZS6JGP
+echo ---------------------------
+
+echo Available commands:
+echo 
+echo $(tput setaf 6)eiab-setup $(tput sgr 0)
+echo "    - Setup, Reinstall and Update your System"
+
+echo $(tput setaf 6)eiab-shell$(tput sgr 0)
+echo "    - Management Shell"
+
+echo $(tput setaf 6)eiab-help$(tput sgr 0)
+echo "    - Help on available commands"
+echo 
+
+echo More info available at https://github.com/JGPorteous/Echolink-In-A-Box/
+echo 
