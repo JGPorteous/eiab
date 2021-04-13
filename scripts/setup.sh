b776e3f75e21cb83176d50ef9ac99a707720017d
Justin Porteous <justin@cloudcoders.co.za>
2021-04-13 20:59:07 +0200

    Install SvxLink Script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,7 +16,7 @@ menu:main:---EchoLink in a Box by ZS6JGP - Setup Menu---
         exec:APT Up_grade::sudo apt upgrade
         nop
         exec:Install _Dependencies:edit,pause:/usr/share/eiab/scripts/install.dependencies.sh
-        show:Install _SvxLink::network
+        show:Install _SvxLink:edit,pause:/usr/share/eiab/scripts/install.svxlink.sh
         nop
         exec:System _Info:edit,display:echo "\n" | cat /var/run/motd.dynamic  | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g'
         exec:CPU _Temperature:truncate:echo CPU: $((`cat /sys/class/thermal/thermal_zone0/temp` / 1000)) Degrees
