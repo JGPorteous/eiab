9a15ae394d8466b3492858802236bc1ea0d0ddb9
Justin Porteous <justin@cloudcoders.co.za>
2021-04-13 18:31:12 +0200

    Base install script

--- install.nanopi.sh
+++ install.nanopi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+clear
+
 echo EiaB :: Installation Script for NanoPi
 
 source /etc/lsb-release
@@ -6,20 +8,20 @@ ARCH=$(uname -m)
 
 if [ "$DISTRIB_ID" != "Ubuntu" ]
 then
-    echo FAILURE: Expected Ubuntu, got $DISTRIB_ID 
+    echo FAILURE: Expected Ubuntu as OS, got $DISTRIB_ID
     exit 1
 fi
 
-if [ "$ARCH" != "armv7l"]
+if [ "$ARCH" != "armv7l" ]
 then
-    echo FAILURE: Expected armv7l, got $ARCH 
+    echo FAILURE: Expected armv7l as architecture, got $ARCH
     exit 1
 fi
 
 REQUIRED_PKG_GIT="git"
 PKG_GIT_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG_GIT|grep "install ok installed")
 
-echo Checking for $REQUIRED_PKG_GIT: $PKG_GIT_OK
+echo Checking for Package: $REQUIRED_PKG_GIT: $PKG_GIT_OK
 if [ "" = "$PKG_GIT_OK" ]; then
   echo "No $REQUIRED_PKG_GIT. Setting up $REQUIRED_PKG_GIT."
   sudo apt-get --yes install $REQUIRED_PKG_GIT 
