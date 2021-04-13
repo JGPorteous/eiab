130d7203f0f8b00553e3b2fa9a75b1a251810703
Justin Porteous <justin@cloudcoders.co.za>
2021-04-13 19:45:42 +0200

    Added Magement Shell

--- install.nanopi.sh
+++ install.nanopi.sh
@@ -71,7 +71,7 @@ usermod echolink --shell /etc/pdmenurc
 echo Setting up commands
 ln -f /usr/share/eiab/scripts/setup.sh /usr/bin/eiab-setup
 ln -f /usr/share/eiab/scripts/help.sh /usr/bin/eiab-help
-ln -f /usr/share/eiab/scripts/shell.sh /etc/pdmenurc
+ln -f /etc/pdmenurc /usr/bin/eiab-shell
 
 echo
 echo
