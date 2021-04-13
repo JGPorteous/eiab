6daf0a78c5dd92e1dabd8d89003ed73c78d29aaa
Justin Porteous <justin@cloudcoders.co.za>
2021-04-13 18:13:47 +0200

    Base install script

--- install.nanopi.sh
+++ install.nanopi.sh
@@ -14,5 +14,19 @@ if [$ARCH != "armv7l"]
     break;
 fi
 
-# Got the right version, let's get the install files...
-#git clone 
+REQUIRED_PKG_GIT="git"
+PKG_GIT_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG_GIT|grep "install ok installed")
+
+echo Checking for $REQUIRED_PKG_GIT: $PKG_GIT_OK
+if [ "" = "$PKG_GIT_OK" ]; then
+  echo "No $REQUIRED_PKG_GIT. Setting up $REQUIRED_PKG_GIT."
+  sudo apt-get --yes install $REQUIRED_PKG_GIT 
+fi
+
+echo Downloading Echolink in a Box
+git clone https://github.com/JGPorteous/Echolink-In-A-Box.git /usr/share/eiab/
+
+cd /usr/share/eiab/
+chmod +x ./setup.sh
+./setup.sh
+
