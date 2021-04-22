2d8fb57522fdb5c1806e112b540b09af2200e466
Justin Porteous <justin@cloudcoders.co.za>
2021-04-22 20:08:27 +0200

    Weather Scripts

--- files/usr/share/eiab/scripts/getweather.sh
+++ files/usr/share/eiab/scripts/getweather.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+currentdir=$(pwd)
+
+mkdir -p /usr/share/eiab/scripts/weather
 cd /usr/share/eiab/scripts/weather 
 rm weather.xml -f 
 wget -O weather.json "http://api.openweathermap.org/data/2.5/weather?id=957655&APPID=dad8f34bb49d6e3ba1b63200028689d5&mode=json&units=metric"
@@ -14,3 +17,5 @@ weather.json > weather.description jq -r '.wind.speed' weather.json >
 wind.speed.value jq -r '.wind.deg' weather.json > wind.deg.value cp -f 
 pressure.value pressure.last.value jq -r '.main.pressure' weather.json > 
 pressure.value jq -r '.main.humidity' weather.json > humidity.value
+
+cd $currentdir
