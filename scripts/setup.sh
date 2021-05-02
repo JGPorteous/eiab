f1bd711a5e521c6eb77066ecd130f3d641fd118c
Justin Porteous <justin@porteous.co.za>
2021-05-02 15:01:22 +0200

    Added Svxlink Google TTS to Setup Menu

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,7 +16,8 @@ menu:main:---EchoLink in a Box by ZS6JGP - Setup Menu---
         exec:APT Up_grade::sudo apt upgrade
         nop
         exec:Install _Dependencies:edit,pause:/usr/share/eiab/scripts/install.dependencies.sh
-        show:Install _SvxLink:edit,pause:/usr/share/eiab/scripts/install.svxlink.sh
+        exec:Install _SvxLink:edit,pause:/usr/share/eiab/scripts/install.svxlink.sh
+        exec:Install SvxLink _Google TTS:edit,pause:/usr/share/eiab/scripts/install.svxlink-google-tts.sh
         nop
         exec:System _Info:edit,display:echo "\n" | cat /var/run/motd.dynamic  | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g'
         exec:CPU _Temperature:truncate:echo CPU: $((`cat /sys/class/thermal/thermal_zone0/temp` / 1000)) Degrees
