d1caf02c67160ba4375b675e18bfb27da4d24ae1
Justin Porteous <justin@porteous.co.za>
2021-05-02 14:57:00 +0200

    Remove old version of svxlink-google-tts

--- scripts/install.svxlink-google-tts.sh
+++ scripts/install.svxlink-google-tts.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+if [ -d "/usr/share/svxlink-google-tts" ]; then
+    echo
+    echo Removing old version of Svxlink Google TTS
+    echo ----------------------------
+    rm -fr /usr/share/svxlink-google-tts
+    echo Done
+    echo
+fi
+
 git clone https://github.com/n7ipb/svxlink-google-tts.git /usr/share/svxlink-google-tts
