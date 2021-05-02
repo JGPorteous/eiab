0f5d59d712315226c5d4469245e8318045106694
Justin Porteous <justin@porteous.co.za>
2021-05-02 13:42:23 +0200

    Fixed getweather script

--- scripts/weather/getweather.sh
+++ scripts/weather/getweather.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 currentdir=$(pwd)
 #Application ID for openweather
-weatherappid=$(cat /usr/share/eiab/scripts/weather.appid)
+weatherappid=$(cat /usr/share/eiab/scripts/weather/weather.appid)
 #ID / location
-weatherid=$(cat /usr/share/eiab/scripts/weather.id)
+weatherid=$(cat /usr/share/eiab/scripts/weather/weather.id)
 
 #dev
-cp ~/Downloads/echolink-in-a-box/files/usr/share/eiab/scripts/getweather.sh /usr/share/eiab/scripts/
-
+#cp ~/Downloads/echolink-in-a-box/files/usr/share/eiab/scripts/getweather.sh /usr/share/eiab/scripts/
 
 mkdir -p /usr/share/eiab/scripts/weather
 cd /usr/share/eiab/scripts/weather 
@@ -16,8 +15,10 @@ echo current directory is $(pwd)
 echo
 
 rm weather.xml -f 
-#wget -O weather.json "http://api.openweathermap.org/data/2.5/weather?id=957655&APPID=dad8f34bb49d6e3ba1b63200028689d5&mode=json&units=metric"
-wget -O weather.json "http://api.openweathermap.org/data/2.5/weather?id=$weatherid&APPID=$weatherappid&mode=json&units=metric"
+url="http://api.openweathermap.org/data/2.5/weather?id=$weatherid&APPID=$weatherappid&mode=json&units=metric"
+ 
+wget -O /usr/share/eiab/scripts/weather/weather.json "${url}"
+
 
 jq -r '.main.temp' weather.json > temp.value 
 jq -r '.main.humidity' weather.json > humidity.value 
@@ -29,7 +30,3 @@ jq -r '.main.pressure' weather.json > pressure.value
 jq -r '.main.humidity' weather.json > humidity.value
 
 cd $currentdir
-
-echo weatherappid $weatherappid
-echo weatherid $weatherid
- echo "http://api.openweathermap.org/data/2.5/weather?id=$weatherid&APPID=$weatherappid&mode=json&units=metric"
