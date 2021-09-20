08fe8c7015fecaf47faab0a373c3acfa154e8a3f
Justin Porteous <justin@porteous.co.za>
2021-09-20 20:25:52 +0200

    Added SSH Configuration

--- scripts/eiab/eiab-version.sh
+++ scripts/eiab/eiab-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo echo $(tput setaf 6)Echolink-In-A-Box$(tput sgr 0) Verion $(tput setaf 5)0.3$(tput sgr 0)
+echo $(tput setaf 6)Echolink-In-A-Box$(tput sgr 0) Verion $(tput setaf 5)0.3$(tput sgr 0)
 echo ""
