de595de01d16c25b4499e65f8f51c999bc8d2c38
Justin Porteous <justin@porteous.co.za>
2021-09-20 19:58:20 +0200

    Updated update for NanoPi, prevent update if in installation folder

--- scripts/update.nanopi.sh
+++ scripts/update.nanopi.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 CURRENT_DIR=$(pwd)
 if [[ $CURRENT_DIR == *"/usr/share/eiab"* ]]; then
-    echo "Cannot update Eacholink in a Box when in folder /usr/share/eiab or any subfolder"
+    echo "Cannot update Echolink in a Box when in folder /usr/share/eiab or any subfolder."
+    echo ""
     exit 0
 fi
 curl -L https://git.io/J38AI | bash
