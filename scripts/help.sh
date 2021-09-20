4464cb78669f55a54b9f2e48f22747827cb19d40
Justin Porteous <justin@porteous.co.za>
2021-09-20 20:03:43 +0200

    Updated eiab-help with update and version commands

--- scripts/help.sh
+++ scripts/help.sh
@@ -13,11 +13,9 @@ echo "    - Management Shell"
 
 echo $(tput setaf 6)eiab-help$(tput sgr 0)
 echo "    - Help on available commands"
-echo 
 
 echo $(tput setaf 6)eiab-update$(tput sgr 0)
 echo "    - Update Echolink in a Box"
-echo 
 
 echo $(tput setaf 6)eiab-version$(tput sgr 0)
 echo "    - Version of Echolink in a Box"
