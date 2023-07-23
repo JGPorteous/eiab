244dfbe4aa44df0f95ef32a93b3dbb2be6a332a9
Justin Porteous <justin@cloudcoders.co.za>
2023-07-23 14:45:07 +0200

    Updated pip upgrade

--- scripts/install.dependencies.nanopi.sh
+++ scripts/install.dependencies.nanopi.sh
@@ -10,4 +10,7 @@ apt -y install make cmake groff gzip doxygen tar git
 apt -y install libsigc++ tcl libgcrypt-dev libasound-dev libspeex-dev librtlsdr-dev libpopt-dev libgsm1-dev  
 apt -y install g++ cmake make libsigc++-2.0-dev libgsm1-dev libpopt-dev tcl-dev libgcrypt20-dev libspeex-dev libasound2-dev libopus-dev librtlsdr-dev doxygen groff alsa-utils vorbis-tools curl libcurl4-openssl-dev rtl-sdr libcurl4-openssl-dev libjsoncpp-dev
 apt -y install python python-pip 
-pip install --upgrade pip
+#pip install --upgrade pip
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+python get-pip.py
+pip install --upgrade setuptools
--- scripts/install.dependencies.raspberrypi.sh
+++ scripts/install.dependencies.raspberrypi.sh
@@ -10,3 +10,6 @@ sudo apt -y install make cmake groff gzip doxygen tar git
 sudo apt -y install libsigc++ tcl libgcrypt-dev libasound-dev libspeex-dev librtlsdr-dev libpopt-dev libgsm1-dev  
 sudo apt -y install g++ cmake make libsigc++-2.0-dev libgsm1-dev libpopt-dev tcl-dev libgcrypt20-dev libspeex-dev libasound2-dev libopus-dev librtlsdr-dev doxygen groff alsa-utils vorbis-tools curl libcurl4-openssl-dev rtl-sdr libcurl4-openssl-dev libjsoncpp-dev
 apt -y install python3 python3-pip
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+python get-pip.py
+pip install --upgrade setuptools
