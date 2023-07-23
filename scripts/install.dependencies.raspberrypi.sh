483a4537a1400912b611c845b7e21cb21d65d22f
Justin Porteous <justin@cloudcoders.co.za>
2023-07-23 15:26:45 +0200

    Added nano to dependencies

--- scripts/install.dependencies.raspberrypi.sh
+++ scripts/install.dependencies.raspberrypi.sh
@@ -4,7 +4,7 @@ echo Dependencies Installation for Rasperry Pi running Raspbian
 echo ----------------------------------------------------------
 echo
 
-sudo apt -y install software-properties-common htop unzip vorbis-tools apache2 jq
+sudo apt -y install software-properties-common htop unzip vorbis-tools apache2 jq nano
 sudo apt -y install php libapache2-mod-php ntpdate
 sudo apt -y install make cmake groff gzip doxygen tar git 
 sudo apt -y install libsigc++ tcl libgcrypt-dev libasound-dev libspeex-dev librtlsdr-dev libpopt-dev libgsm1-dev  
