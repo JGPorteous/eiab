7452af8a787afa57477ae56155d001f539a53295
Justin Porteous <justin@porteous.co.za>
2023-08-20 12:59:18 +0200

    Update radio_manager.py

--- scripts/sa818/radio_manager.py
+++ scripts/sa818/radio_manager.py
@@ -3,14 +3,17 @@ import serial
 import argparse
 
 port = '/dev/ttyS1'
+commands = {}
+commands['getVersion'] = b'AAFAA'
+commands['getSettings'] = b'AAFA1'
+commands['setSettings'] = b'AAF'
 
 parser = argparse.ArgumentParser(description='NiceRF SA818/SA828 Radio Manager.')
-parser.add_argument('-c', '--command')
-#parser.add_argument('-c', '--current_settings')
-
+parser.add_argument('-c', '--command',help='Available Commands: getVersion, getSettings, setSettings', required=True)
 
 args = parser.parse_args()
-#print(args)
+print(args)
+print(args.command)
 
 def sendCommand(command):
         ser = serial.Serial(port, 9600, timeout=1)
@@ -21,11 +24,10 @@ def sendCommand(command):
         print("Return: ", end='')
         print(read_val)
 
+if (args.command == 'getVersion'):
+        print('Getting Version...')
+        sendCommand(commands['getVersion'])
 
-
-print(args.command)
-if (args.command == 'version'):
-        sendCommand(b'AAFAA')
-if (args.command == 'current_settings'):
-        sendCommand(b'AAFA1')
-
+if (args.command == 'getSettings'):
+        print('Getting Settings...')
+        sendCommand(commands[args.command])
